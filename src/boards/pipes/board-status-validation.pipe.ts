399211a41258fe11302612000a34474644b48392
Charming (차밍) <charming@inflearn.com>
2021-10-20 09:54:26 +0900

    feat: 커스텀 validation 파이프 구현

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,8 +1,24 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata): any {
-    console.log(value);
-    console.log(metadata);
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+
+  transform(value: any): any {
+    if (this.isNotExistBoardId(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  isNotExistBoardId(value: any) {
+    const index = this.StatusOptions.indexOf(value);
+
+    return index !== -1;
   }
 }
