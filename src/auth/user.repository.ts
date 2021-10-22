247635961fca9219d7b559a730e34815f28250da
Charming (차밍) <charming@inflearn.com>
2021-10-22 21:10:46 +0900

    new: UserRepository createUser() 선언

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,16 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = await this.create({
+      username,
+      password,
+    });
+
+    await this.save(user);
+  }
+}
